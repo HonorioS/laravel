cbfdfb0af80f134dfbad3ae882f0b6247715fe59
HonorioS <dasilvah77@gmail.com>
2021-06-20 22:09:30 +0100

    Route with ID

--- resources/views/produtos/show.blade.php
+++ resources/views/produtos/show.blade.php
@@ -2,13 +2,26 @@
 @section('title', 'Mostrar produtos')
 @section('content')
 
+<?php
+
+$format_value = number_format($produto->valor, 2, ',', '.');
+?>
 
 <!-- jumbotron v5 -->
 <div class="bg-light p-5 rounded-lg m-3">
-    <h1 class="display-4">   <?php echo $produto->Nome ?></h1>
+    <h1 class="display-4"> <?php echo $produto->Nome ?> - Valor € <?php echo $format_value ?> </h1>
     <p class="lead"><?php echo $produto->descricao ?></p>
     <hr class="my-4">
-    <p>It uses utility classes for r container.</p>
+    <p>
+        Lorem Ipsum is simply dummy text of the printing and
+        typesetting industry. Lorem Ipsum has been the industry's
+        standard dummy text ever since the 1500s,
+        when an unknown printer took a galley of type and scrambled it to make
+        Lorem Ipsum is simply dummy text of the printing and
+        typesetting industry. Lorem Ipsum has been the industry's
+        standard dummy text ever since the 1500s,
+        when an unknown printer took a galley of type and scrambled it to make
+    </p>
     <a class="btn btn-primary btn-lg" href="{{route('produtos')}}" role="button"> Ver Produtos</a>
 </div>
 
