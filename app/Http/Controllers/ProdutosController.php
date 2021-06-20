c8794bf154db138479f81585f67063a3c7902355
HonorioS <dasilvah77@gmail.com>
2021-06-20 13:26:38 +0100

    new files added

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\produto;
 use Illuminate\Http\Request;
 
 class ProdutosController extends Controller
@@ -9,28 +10,29 @@ class ProdutosController extends Controller
 
     public function index()
     {
-        //return "pagina principal produtos";
-        return view('produtos.index');
+        $produto = produto::all();
+        return $produto;
+
+        // return view('produtos.index');
 
     }
 
     public function create()
     {
 
-       // return "pagina para criar  produtos";
+        // return "pagina para criar  produtos";
         return view('produtos.create');
-
     }
 
     public function show($nome, $valor)
     {
 
-        return view('produtos.show',[ 'nome' => $nome, 'valor' => $valor ]);
+        return view('produtos.show', ['nome' => $nome, 'valor' => $valor]);
 
         // if ($nome) {
-            // return "o nome do produto é $nome, e o  seu valor é  $valor €";
+        // return "o nome do produto é $nome, e o  seu valor é  $valor €";
         // } else {
-            // return -1;
+        // return -1;
         // }
     }
 }
