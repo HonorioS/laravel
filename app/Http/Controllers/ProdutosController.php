e6ef97ee3fbb82d923207e5b63994d5025556c29
HonorioS <dasilvah77@gmail.com>
2021-06-20 12:37:37 +0100

    update file

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -6,22 +6,28 @@ use Illuminate\Http\Request;
 
 class ProdutosController extends Controller
 {
-    
-    public function index(){
+
+    public function index()
+    {
 
 
         return "pagina principal produtos";
     }
 
-    public function create(){
-    
-        return "pagina para criar  produtos";
+    public function create()
+    {
 
+        return "pagina para criar  produtos";
     }
 
-    public function show(){
-    
-        return "pagina para exibir produtos";
+    public function show($nome, $valor)
+    {
 
+
+        if ($nome) {
+            return "o nome do produto é $nome, e o  seu valor é  $valor ";
+        } else {
+            return -1;
+        }
     }
 }
